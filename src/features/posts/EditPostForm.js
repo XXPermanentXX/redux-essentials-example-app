2aa5c43a2a91be00648111968cbe01a90106cc9d
Shiqi Gong <92624196+XXPermanentXX@users.noreply.github.com>
2024-03-21 23:48:24 +0100

    Part 5: Async Logic and Data Fetching

--- src/features/posts/EditPostForm.js
+++ src/features/posts/EditPostForm.js
@@ -2,16 +2,15 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 
-import { postUpdated } from './postsSlice'
+import { postUpdated, selectPostById } from './postsSlice'
 
 export const EditPostForm = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector((state) =>
-    state.posts.find((post) => post.id === postId),
-  )
+  const post = useSelector((state) => selectPostById(state, postId))
 
   const dispatch = useDispatch()
+  const history = useHistory()
 
   const [title, setTitle] = useState(post.title)
   const [content, setContent] = useState(post.content)
@@ -21,7 +20,7 @@ export const EditPostForm = ({ match }) => {
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(postId, title, content)
+      dispatch(postUpdated(postId, title, content))
       history.push(`/posts/${postId}`)
     }
   }
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,22 +1,27 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
-import { sub } from 'date-fns'
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
-const initialState = [
-  {
-    id: '1',
-    title: 'First Post!',
-    content: 'Hello!',
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null,
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.data
+})
+
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  // The payload creator receives the partial `{title, content, user}` object
+  async (initialPost) => {
+    // We send the initial data to the fake API server
+    const response = await client.post('/fakeApi/posts', initialPost)
+    // The response includes the complete post object, including unique ID
+    return response.data
   },
-  {
-    id: '2',
-    title: 'Second Post',
-    content: 'More text',
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
-  },
-]
+)
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -24,27 +29,29 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload)
+        state.posts.push(action.payload)
       },
       prepare(title, content, userId) {
         return {
           payload: {
             id: nanoid(),
-            date: new Date().toISOString(),
             title,
             content,
+            date: new Date().toISOString(),
             user: userId,
+            reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
           },
         }
       },
     },
     postUpdated: {
       reducer(state, action) {
-        const { id, title, content } = action.payload
-        const existingPost = state.find((post) => post.id === id)
+        const { id, title, content, date } = action.payload
+        const existingPost = state.posts.find((post) => post.id === id)
         if (existingPost) {
           existingPost.title = title
           existingPost.content = content
+          existingPost.date = date
         }
       },
       prepare(postId, title, content) {
@@ -53,19 +60,44 @@ const postsSlice = createSlice({
             id: postId,
             title,
             content,
+            date: new Date().toISOString(),
           },
         }
       },
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload
-      const existingPost = state.find((post) => post.id === postId)
+      const existingPost = state.posts.find((post) => post.id === id)
       if (existingPost) {
         existingPost.reactions[reaction]++
       }
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        // We can directly add the new post object to our posts array
+        state.posts.push(action.payload)
+      })
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  },
 })
 
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 export default postsSlice.reducer
+
+export const selectAllPosts = (state) => state.posts.posts
+
+export const selectPostById = (state, postId) =>
+  state.posts.posts.find((post) => post.id === postId)
